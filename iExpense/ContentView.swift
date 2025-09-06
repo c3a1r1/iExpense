f59de042fdbb60bdd910443debda1e754214ff52
= <=>
2025-09-06 09:45:25 -0400

    Binding tutorial

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var user = User()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Your name is \(user.firstName) \(user.lastName)")
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
         }
         .padding()
     }
---
+++ iExpense/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  iExpense
+//
+//  Created by Carmine on 9/4/25.
+//
+
+import Foundation
+import Observation
+
+@Observable
+class User{
+    var firstName = "Bilbo"
+    var lastName = "Baggins"
+}
